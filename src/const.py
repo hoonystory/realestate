4e41da572e9017ecd6cd9dc23c2b1a0b4cf545df
hoony <sylcjnas@icloud.com>
2023-06-03 19:50:48 +0900

    set accessKey in private

--- src/const.py
+++ src/const.py
@@ -1,14 +1,22 @@
+from src.utils import file_path
+
+
+def get_access_key():
+    root_path = file_path.get_project_root_path()
+    f = open(root_path + '/resources/private/accessKey', 'r')
+    access_key = f.readline()
+    f.close()
+    return access_key
+
+
 def get_request_info(key):
     return api_info.get(key, '')
 
 
+access_key = get_access_key()
 api_info = {
-    'access_key':
-        'Ak5ExMq7svgb45fKdmgOJOaWOOrp5KAa7LUiJ1fc7cRujpLh5bVWLtnfzd1ft0nuLT0nZiTv9T1u7vLoVw5Kzg==',
-    'request_url':
-        'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc',
-    'real_txn_apt_trade':
-        '/getRTMSDataSvcAptTrade',
-    'real_txn_sht_trade':
-        '/getRTMSDataSvcSHTrade'
+    'access_key': access_key,
+    'request_url': 'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc',
+    'real_txn_apt_trade': '/getRTMSDataSvcAptTrade',
+    'real_txn_sht_trade': '/getRTMSDataSvcSHTrade'
 }
